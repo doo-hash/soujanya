9a9975818fb864c805129a968b9cc8b13bb7f683
Maachannagari Soujanya <soujanyamachanna@gmail.com>
2021-04-17 22:15:54 +0530

    form td

--- angular/tdformsapp/src/app/app.component.ts
+++ angular/tdformsapp/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tdformsapp';
+  model=new user();
+  onSubmit(form:any){
+    console.log(form.value);
+  }
+}
+
+export class user{
+  public fname:string='';
+  public lname:string='';
+  public emailid:string='';
+  public dob:string='';
+  public age?:number;
+  public gndr:string='';
+  public mobile?:number;
+  public psd:string='';
+  public langs1:string='';
+  public proglang:string='';
+  public lksdslks:string='';
 }
